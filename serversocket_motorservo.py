d566f15f14ffed10a50af54351bd7e50428c7b83
mango-lassi <124420360+mango-lassi@users.noreply.github.com>
2023-04-17 14:34:52 -0400

    Create serversocket_motorservo.py
    tested on apr 17; blew out the resistor but the socket worked and took in keyboard inputs relayed to the BB

---
+++ serversocket_motorservo.py
@@ -0,0 +1,112 @@
+# first of all import the socket library
+import socket
+import time
+import Adafruit_BBIO.PWM as PWM
+DRIVE_PIN = "P9_16"
+SERVO_PIN = "P9_14"
+
+#starting position
+#PWM.start(DRIVE_PIN,7.5,50)
+PWM.start(SERVO_PIN,3,50)
+#____________________________
+
+def drive(num):
+    if 10.0 >= num >= 5.0:
+        PWM.set_duty_cycle(DRIVE_PIN, num)
+
+
+def calibrate():
+#def calibrate(inp):
+    PWM.set_duty_cycle(DRIVE_PIN, 0.0)
+    print("Make sure battery is connected but switch is OFF.  Press ENTER to continue")
+    inp = input()
+    if inp == '':
+        PWM.set_duty_cycle(DRIVE_PIN, 10.0)
+        print("Turn the switch ON now. You will hear two beeps then press Enter")
+        inp = input()
+        if inp == '':            
+            PWM.set_duty_cycle(DRIVE_PIN, 5.0)
+            print ("Working...")
+            time.sleep(7)
+            print ("Wait for it ....")
+            time.sleep (5)
+            print ("Almost there.....")
+            PWM.set_duty_cycle(DRIVE_PIN, 0.0)
+            time.sleep(2)
+            print ("Arming ESC now...")
+            PWM.set_duty_cycle(DRIVE_PIN, 5.0)
+            time.sleep(1)
+            print ("ESC is armed (You should have heard another beep).  You can now drive the car")
+            PWM.set_duty_cycle(DRIVE_PIN, 7.5)
+            print("I have finished running\n")
+
+def main():
+
+# next create a socket object
+    s = socket.socket()
+    print ("Socket successfully created")
+
+# reserve a port on your computer in our
+# case it is 12345 but it can be anything
+    port = 55554
+
+# Next bind to the port
+# we have not typed any ip in the ip field
+# instead we have inputted an empty string
+# this makes the server listen to requests
+# coming from other computers on the network
+    s.bind(('', port))
+    print ("socket binded to %s" %(port))
+
+# put the socket into listening mode
+    s.listen(5)
+    print ("socket is listening")
+
+# a forever loop until we interrupt it or
+# an error occurs
+    while True:
+
+# Establish connection with client.
+        c, addr = s.accept()
+        print ('Got connection from', addr )
+
+# send a thank you message to the client. encoding to send byte type.
+        c.send('Thank you for connecting'.encode())
+        print("here what I got: ")
+        calibrate()
+        #calibrate('')
+        buf = ''
+        direction = "i"
+
+        servo_pos = 3
+        drive_pos = 7.5
+
+        while(direction != "q"):
+            buf = c.recv(1024)
+            strings = buf.decode('utf8')
+            #strings = buf.decode()
+            print("the data is "+strings)
+            #num = char(strings)
+            direction = strings
+            print(direction)
+            #drive(num)
+            if(direction == "a"): #left
+                servo_pos += 0.05
+                servo_cycle = (0.055*(float(servo_pos)) + 3)
+                PWM.set_duty_cycle(SERVO_PIN, servo_cycle)
+            elif(direction == "d"): #right
+                servo_pos -= 0.05
+                servo_cycle = (0.055*(float(servo_pos)) + 3)
+                PWM.set_duty_cycle(SERVO_PIN, servo_cycle)
+            elif(direction == "w"): #forward
+                drive_pos += 0.05
+                if 10.0 >= drive_pos >= 5.0:
+                    PWM.set_duty_cycle(DRIVE_PIN, drive_pos)
+            elif(direction == "s"): #backward
+                drive_pos -= 0.05
+                if 10.0 >= drive_pos >= 5.0:
+                    PWM.set_duty_cycle(DRIVE_PIN, drive_pos)
+
+        c.close()
+        break
+main()
